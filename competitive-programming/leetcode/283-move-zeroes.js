b41f604676f65025eb792e3d9646dde80ef6487d
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2023-02-16 12:37:15 +0530

    feat: leetcode problem 283

---
+++ competitive-programming/leetcode/283-move-zeroes.js
@@ -0,0 +1,51 @@
+/*
+https://leetcode.com/problems/move-zeroes/
+
+283. Move Zeroes
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+
+
+Follow up: Could you minimize the total number of operations done?
+ */
+
+const moveZeroes = (nums) => {
+  let low = 0;
+  let high = low + 1;
+
+  while (high <= nums.length - 1) {
+    if (nums[low] !== 0) {
+      low++;
+      high++;
+    } else {
+      if (nums[high] !== 0) {
+        [nums[low], nums[high]] = [nums[high], nums[low]];
+        low++;
+      }
+      high++;
+    }
+  }
+
+  return nums;
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]))
